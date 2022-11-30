94011bd3dac0bf3cf186b4a6c9daff69694837f6
João Neto <dev3up@Joaos-MacBook-Air.local>
2022-11-29 23:05:19 -0300

    Fix bugs

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ app.post("/repositories", (request, response) => {
     techs,
     likes: 0
   };
+  repositories.push(repository);
 
   return response.json(repository);
 });
@@ -29,13 +30,12 @@ app.post("/repositories", (request, response) => {
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
-
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  delete updatedRepository.likes
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
-
   const repository = { ...repositories[repositoryIndex], ...updatedRepository };
 
   repositories[repositoryIndex] = repository;
@@ -48,7 +48,7 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -66,9 +66,8 @@ app.post("/repositories/:id/like", (request, response) => {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const likes = ++repositories[repositoryIndex].likes;
-
-  return response.json('likes');
+  repositories[repositoryIndex].likes += 1
+  return response.json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
